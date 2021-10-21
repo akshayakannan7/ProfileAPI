9dea7e7054ebd4280ac0c1871d671cf9ee9bfe4a
unknown <Administrator@KANINI-LTP-485.gisbizdev.gisbiz.chennai.com>
2021-10-21 21:23:47 +0530

    first commit

---
+++ ProfileAPI/Models/OrderFinal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProfileAPI.Models
+{
+    public partial class OrderFinal
+    {
+        public int Orderid { get; set; }
+        public int? Userid { get; set; }
+        public int? Productid { get; set; }
+        public string ProductImage { get; set; }
+        public string Productname { get; set; }
+        public string ProductDesc { get; set; }
+        public float Price { get; set; }
+        public DateTime? DateofPurchase { get; set; }
+    }
+}
